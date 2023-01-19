25772fd8465849065d0b992e4784a013a8d3b00e
ry10an23 <kawaguchi.r823@gmail.com>
2023-01-19 12:37:51 -0800

    hamburger menu

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 "use strict";
+
+/*=================================================
+    HAMBURGER MENU
+===================================================*/
+function hamburgerMenu() {
+  const logoImg = document.querySelector("#logoPic");
+  const imgUrl = "./img/logo.png";
+  const imgUrlReverse = "./img/logo_reverse.png";
+  let navBar = document.querySelector("#nav");
+  let hamburgerBtn = document.querySelector(".hamburger");
+
+  if (
+    !(
+      navBar.classList.contains("active") &&
+      hamburgerBtn.classList.contains("active")
+    )
+  ) {
+    navBar.classList.add("active");
+    hamburgerBtn.classList.add("active");
+    logoImg.src = imgUrlReverse;
+  } else {
+    navBar.classList.remove("active");
+    hamburgerBtn.classList.remove("active");
+    logoImg.src = imgUrl;
+  }
+}
