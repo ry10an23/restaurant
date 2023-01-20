0af416ba91592711de72497e9aef5fc454cfe6ae
ry10an23 <kawaguchi.r823@gmail.com>
2023-01-19 16:13:34 -0800

    rescaling img size code for main visual

--- index.js
+++ index.js
@@ -25,3 +25,29 @@ function hamburgerMenu() {
     logoImg.src = imgUrl;
   }
 }
+
+window.addEventListener("scroll", function () {
+  let scroll = window.pageYOffset;
+  mv_scale(scroll);
+});
+
+/*=================================================
+    RESCALING MAIN VISUAL SCALING (COMMON PROCESS)
+===================================================*/
+const mv_scale = () => {
+  window.addEventListener("scroll", () => {
+    if (window.innerWidth > 900) {
+      const currentY = window.pageYOffset;
+      let imgs = document.getElementsByClassName("main_pic");
+      for (const img of imgs) {
+        img.style.width = 100 / 3 + currentY / 10 + "%";
+      }
+    } else {
+      const currentY = window.pageYOffset;
+      let imgs = document.getElementsByClassName("main_pic");
+      for (const img of imgs) {
+        img.style.width = 100 - currentY / 10 + "%";
+      }
+    }
+  });
+};
