7b5fd1943c92aaff65fa78cbad279efdf14638cb
ry10an23 <kawaguchi.r823@gmail.com>
2023-02-01 12:51:39 -0800

    add single page application

--- menu.js
+++ menu.js
@@ -33,3 +33,34 @@ window.addEventListener("load", () => {
   menu_title.classList.contains("fadein");
   menu_title.classList.add("show");
 });
+
+/*=================================================
+    SPA
+===================================================*/
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
+};
+
+const routes = {
+  404: "/pages/404.html",
+  "/pasta/": "/pages/pasta.html",
+  "/appetizer/": "/pages/appetizer.html",
+  "/dessert/": "/pages/dessert.html",
+  "/drink/": "/pages/drink.html",
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("main_page").innerHTML = html;
+};
+
+window.addEventListener("popstate", (event) => {
+  handleLocation();
+});
+
+handleLocation();
