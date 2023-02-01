665b1049efbad6e7bb60928d70ceaf49e877af21
ry10an23 <kawaguchi.r823@gmail.com>
2023-02-01 11:43:30 -0800

    addd hamburger menu

--- menu.js
+++ menu.js
@@ -1,1 +1,21 @@
 "use strict";
+/*=================================================
+    HAMBURGER MENU
+===================================================*/
+function hamburgerMenu() {
+  let navBar = document.querySelector("#nav");
+  let hamburgerBtn = document.querySelector(".hamburger");
+
+  if (
+    !(
+      navBar.classList.contains("active") &&
+      hamburgerBtn.classList.contains("active")
+    )
+  ) {
+    navBar.classList.add("active");
+    hamburgerBtn.classList.add("active");
+  } else {
+    navBar.classList.remove("active");
+    hamburgerBtn.classList.remove("active");
+  }
+}
